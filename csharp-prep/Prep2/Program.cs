60307c4b5b7ac98b9803e68a93af354b31c6d24c
osoriosam <104704268+osoriosam@users.noreply.github.com>
2023-04-22 22:54:13 -0400

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        string sign = "";
+        int x;
+        int remainder;
+        Console.WriteLine("Hello, what is your grade pecentage?");
+        string percentage_grade = Console.ReadLine();
+        x = int.Parse(percentage_grade);
+        if (x < 60)
+        {
+            letter = "F";
+        }
+        else if(x < 70)
+        {
+            letter = "D";
+        }
+        else if(x < 80)
+        {
+            letter = "C";
+        }
+        else if(x < 90)
+        {
+            letter = "B";
+        }
+        else
+        {
+            letter = "A";
+        }
+        remainder = x % 10;
+        if (remainder < 3)
+        {
+            sign = "-";
+        }
+        else if (remainder >=7)
+        {
+            sign = "+";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        if (letter == "A")
+        {
+            if(sign == "+")
+            {
+                sign = "";
+            }
+        }
+        Console.WriteLine($"Your letter grade is {letter}{sign}");
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately this time you don't passed, but keep trying and good luck next time");
+        }
     }
 }
